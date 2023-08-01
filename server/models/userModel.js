87de7c79d16e6fe9b4cf7b09b1655305ae5ea41c
Bartosz Bialokozowicz <b.bialokozowicz@gmail.com>
2023-08-02 01:24:23 +0200

    added /drinks/users add user to mongoDB

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -8,6 +8,6 @@ const UserSchema = new Schema({
     city: String,
     email: String,
     password: String,
-});
+}, {timestamps: true});
 
 export default model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
-import User from "./models/User.js";
 import cors from 'cors';
+import router from './routes/drinks.js'
 
 const { MONGO_URL, PORT } = process.env;
 
@@ -20,79 +20,93 @@ app.use((req,res,next) => {
   next()
 })
 
-(async () => {
-  try {
-    await mongoose.connect(MONGO_URL, {});
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server running on: http://localhost:${PORT}`);
-    });
-  } catch (error) {
+mongoose.connect(MONGO_URL)
+.then(()=> {
+  app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+      })
+    })
+   .catch ((error) => {
     console.error(error);
     app.close();
-  }
-})();
+   })
 
-app.post( `/signup`, async (req, res) => {
-  try {
-      const name = req.body.name;
-      const surname = req.body.surname;
-      const username = req.body.username;
-      const city = req.body.city;
-      const email = req.body.email;
-      const password = req.body.password;
-      const user = new User ({
-         name,
-         surname,
-         username,
-         city,
-         email,
-         password,
-      })
-      await user.save();
-      res.status(200).json({success: true});
-  } catch (error) {
-      console.log(error)
-      res.status(500).json({ success: false, error: "Failed" })
-  }
-  }
-  )
+app.use('/drinks', router)
+  
+export default app  
 
-app.get('/', (req, res) => {
-  res.redirect('/mainpage');
-});
+// (async () => {
+//   try {
+//     await mongoose.connect(MONGO_URL, {});
+//     console.log('Connected to MongoDB');
+//     app.listen(PORT, () => {
+//       console.log(`Server running on: http://localhost:${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     app.close();
+//   }
+// })();
+  
+//   try {
+//       const name = req.body.name;
+//       const surname = req.body.surname;
+//       const username = req.body.username;
+//       const city = req.body.city;
+//       const email = req.body.email;
+//       const password = req.body.password;
+//       const user = new User ({
+//          name,
+//          surname,
+//          username,
+//          city,
+//          email,
+//          password,
+//       })
+//       await user.save();
+//       res.status(200).json({success: true});
+//   } catch (error) {
+//       console.log(error)
+//       res.status(500).json({ success: false, error: "Failed" })
+//   }
+//   }
+//   )
 
-app.get('/mainpage', (req, res) => {
-  res.send('MainPage');
-});
+// app.get('/', (req, res) => {
+//   res.redirect('/mainpage');
+// });
 
-app.get('/register', (req, res) => {
-  res.send('Register');
-});
+// app.get('/mainpage', (req, res) => {
+//   res.send('MainPage');
+// });
 
-app.get('/user', (req, res) => {
-  res.send('user settings');
-});
+// app.get('/register', (req, res) => {
+//   res.send('Register');
+// });
 
-app.get('/user/mainpage', (req, res) => {
-  res.send('user mainpage');
-});
+// app.get('/user', (req, res) => {
+//   res.send('user settings');
+// });
 
-app.get('/user/favorites', (req, res) => {
-  res.send('favorites');
-});
+// app.get('/user/mainpage', (req, res) => {
+//   res.send('user mainpage');
+// });
 
-app.get('/drink', (req, res) => {
-  res.send('drink');
-});
+// app.get('/user/favorites', (req, res) => {
+//   res.send('favorites');
+// });
 
-app.get('/drink/:id', (req, res) => {
-  res.send('drink by name');
-});
+// app.get('/drink', (req, res) => {
+//   res.send('drink');
+// });
+
+// app.get('/drink/:id', (req, res) => {
+//   res.send('drink by name');
+// });
 
 
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
+// app.use((req, res) => {
+//   res.status(404).send('Not Found');
+// });
 
 
