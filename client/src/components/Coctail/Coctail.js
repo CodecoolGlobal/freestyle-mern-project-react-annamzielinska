f58437c27b4b97bf96a60f41dd6f00189eb7e808
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-08-01 14:29:55 +0200

    fetched drink and display manuals and picture

--- client/src/components/Coctail/Coctail.js
+++ client/src/components/Coctail/Coctail.js
@@ -1,16 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Coctail() {
-    const { coctailId } = useParams()
+    const { coctailId } = useParams();
+    const [coctail, setCoctail] = useState({});
+
     useEffect(() => {
-        fetch()
-        console.log(coctailId)
-    }, []);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${coctailId}`
+                );
+                const jsonData = await response.json();
+                console.log(jsonData);
+                setCoctail(jsonData.drinks[0]);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, [coctailId]);
 
     return (
         <div>
-            <Coctail />
+            { }
+            <h2>{coctail.strDrink}</h2>
+            <img src={coctail.strDrinkThumb} alt={`${coctail.strDrink}`} />
+            <p>{coctail.strInstructions}</p>
+            { }
         </div>
     );
 }
