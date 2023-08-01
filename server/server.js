4cc06e0b9586ac5d34913c993680a6f05e1f1f46
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-08-01 10:01:37 +0200

    setup server endpoints deleted from client unnecessary files

--- server/server.js
+++ server/server.js
@@ -1,35 +1,66 @@
-import "dotenv/config";
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors"
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const { MONGO_URL, PORT } = process.env;
 
 const corsPolicy = {
-    origin: "http://localhost:3000",
-    headers: "Content-Type, Authorization",
-    method: "GET, POST, PUT, DELETE"
-}
+  origin: 'http://localhost:3000',
+  allowedHeaders: 'Content-Type, Authorization',
+  methods: 'GET, POST, PUT, DELETE',
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsPolicy));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("something");
+app.get('/', (req, res) => {
+  res.redirect('/mainpage');
+});
+
+app.get('/mainpage', (req, res) => {
+  res.send('MainPage');
+});
+
+app.get('/register', (req, res) => {
+  res.send('Register');
+});
+
+app.get('/user', (req, res) => {
+  res.send('user settings');
+});
+
+app.get('/user/mainpage', (req, res) => {
+  res.send('user mainpage');
+});
+
+app.get('/user/favorites', (req, res) => {
+  res.send('favorites');
+});
+
+app.get('/drink', (req, res) => {
+  res.send('drink');
+});
+
+app.get('/drink/:id', (req, res) => {
+  res.send('drink by name');
+});
+
+// Catch-all middleware for handling 404 errors
+app.use((req, res) => {
+  res.status(404).send('Not Found');
 });
 
 (async () => {
-    try {
-        await mongoose.connect(MONGO_URL, {
-            serverSelectionTimeoutMS: 5000,
-        });
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        });
-    }
-    catch (error) {
-        console.error(error);
-        app.close();
-    }
-})
+  try {
+    await mongoose.connect(MONGO_URL, {});
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    app.close();
+  }
+})();
