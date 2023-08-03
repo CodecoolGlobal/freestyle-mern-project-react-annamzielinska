7739b9314abdb592ff4cbfaa0f0efa1e902a9024
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-08-03 12:10:35 +0200

    random drinks css

--- client/src/components/RandomDrink/RandomDrink.js
+++ client/src/components/RandomDrink/RandomDrink.js
@@ -6,7 +6,7 @@ function RandomDrink() {
   const fetchRandom = async () => {
     try {
       const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
-      if (!response.ok) { 
+      if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
       const jsonData = await response.json();
@@ -23,10 +23,12 @@ function RandomDrink() {
   return (
     <>
       {random.drinks.length >= 1 ? (
-        <div>
-          <h3>{random.drinks[0].strDrink}</h3>
-          <img src={random.drinks[0].strDrinkThumb} alt={`${random.drinks[0].strDrink}`} style={{ width: "30vh" }} />
-          <p>{random.drinks[0].strInstructions}</p>
+        <div class="card" style={{ width: "18rem" }}>
+          <img className="card-img-top" src={random.drinks[0].strDrinkThumb} alt={`${random.drinks[0].strDrink}`} />
+          <div className="card-body">
+            <h5 class="card-title" style={{ overflow: "hidden", height: "2.5rem" }}>{random.drinks[0].strDrink}</h5>
+            <p class="card-text" style={{ overflow: "hidden", height: "6rem" }}>{random.drinks[0].strInstructions}</p>
+          </div>
         </div>
       ) : (
         <div>No random drinks available</div>
@@ -34,5 +36,4 @@ function RandomDrink() {
     </>
   );
 }
-
-export default RandomDrink;
+export default RandomDrink
