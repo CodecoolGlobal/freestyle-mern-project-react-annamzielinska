bd26c9a5fe35605e4550a87bad74fe219464be32
Bartosz Bialokozowicz <b.bialokozowicz@gmail.com>
2023-08-03 12:33:34 +0200

    added new methods to user/register server

--- server/models/favourites.js
+++ server/models/favourites.js
@@ -3,11 +3,9 @@ const {Schema, model} = mongoose;
 
 const FavouriteSchema = new Schema({
     name: String,
-    surname: String,
-    username: {type: String, required: true, index: { unique: true }},
-    city: String,
-    email: String,
-    password: String,
+    isAlcoholic: Boolean,
+    ingridients: Array,
+    instructions: String,
 }, {timestamps: true});
 
 export default model('Favourite', FavouriteSchema);
