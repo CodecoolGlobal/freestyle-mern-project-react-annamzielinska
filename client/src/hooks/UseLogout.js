06801dec0be42d4a3226291b1b48eca096c35476
KacperDeja <81015531+KacperDeja@users.noreply.github.com>
2023-08-31 09:27:04 +0200

    ready to merge

--- client/src/hooks/UseLogout.js
+++ client/src/hooks/UseLogout.js
@@ -4,12 +4,12 @@ export const useLogout = () => {
   const { dispatch } = AuthContext()
 
   const logout = () => {
-    // remove user from storage
+
     localStorage.removeItem('user')
 
-    // dispatch logout action
     dispatch({ type: 'LOGOUT' })
   }
 
   return { logout }
+  
 }
